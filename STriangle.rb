a30d8d65c113baa781896b7c4ea3f4aa42471995
TailsSavarin <Alexql7@gmail.com>
2020-01-21 14:07:00 -0800

    2.1

--- STriangle.rb
+++ STriangle.rb
@@ -1,7 +1,7 @@
 puts "For calculating the area of the triangle, I need to know his base and height in centimeters."
 print "Write here, what is his height: "
-h = gets.chomp
+h = gets.to_i
 print "Good, now we need his base: "
-a = gets.chomp
-puts "Good job, the area of a triangle is:  #{(1.0/2)*a.to_i*h.to_i} centimeters."
+a = gets.to_i
+puts "Good job, the area of a triangle is:  #{(1.0/2)*a*h} centimeters."
 
