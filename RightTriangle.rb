72ff8b69dd05e0fbe7dbbf66405b7e02c3c80979
TailsSavarin <Alexql7@gmail.com>
2020-01-21 14:42:16 -0800

    4

--- RightTriangle.rb
+++ RightTriangle.rb
@@ -1,14 +1,20 @@
 puts "Hello, let's find out if the triangle is rectangular, isosceles or equilateral, write the values of the three his sides"
 print "1st side: "
-a = gets.chomp
+a = gets.to_i
 print "2nd side: "
-b = gets.chomp
+b = gets.to_i
 print "3d side: "
-c = gets.chomp
-if a == b && a == c
+c = gets.to_i
+
+#Add array
+sides = [a, b, c].sort!
+
+if sides[2]**2 == sides[1]**2 + sides[0]**2
+  puts "Triangle is recangular."
+elsif a == b && a == c
   puts "Triangle is equilateral and isosceles."
 elsif a == b || a == c || b == c
   puts "Triangle is isosceles."
-elsif 
+else
+  puts "Triangle not is rectangular, isosceles or equilateral"
 end 
-
