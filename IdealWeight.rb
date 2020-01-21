24aa4bbecb2cc7a15d97b0b28d4ead4f2f9f3ae4
TailsSavarin <Alexql7@gmail.com>
2020-01-21 12:44:39 -0800

    1st parth of the lesson

---
+++ IdealWeight.rb
@@ -0,0 +1,18 @@
+puts "Please enter you name"
+name = gets.chomp.capitalize
+puts "Hello #{name}, I'm a program that calculates your ideal weight, wanna know it, write yes or no!"
+answer = gets.chomp.downcase
+case answer
+when 'yes'
+  puts "Good, tell me, how tall are you in centimeters?"
+  height = gets.chomp
+  if (height.to_i - 110)*1.15 > 0
+    puts "#{name}, your ideal weight is #{(height.to_i - 110)*1.15}"
+  else
+    puts "Woops, your weight is already optimal, don't worry about it!"
+  end
+when 'no'
+  puts "Well, goodbye then!"
+else
+  puts "Sorry, I didn't understand you."
+end
