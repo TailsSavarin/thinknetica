a8f9c4a1ae3fb02c1da705c71058fab3e5fd609c
TailsSavarin <Alexql7@gmail.com>
2020-01-22 02:00:28 -0800

    fixed 1

--- ideal_weight.rb
+++ ideal_weight.rb
@@ -1,4 +1,4 @@
-puts 'Please enter you name'
+print 'Please enter you name: '
 name = gets.chomp.capitalize
 puts "Hello #{name}, I'm a program that calculates your ideal weight, wanna know it, write yes or no!"
 answer = gets.chomp.downcase
@@ -7,9 +7,9 @@ when 'yes'
   puts 'Good, tell me, how tall are you in centimeters?'
   height = gets.to_i
   #Add variable
-  ideal_weight = (height - 110)*1.15
+  ideal_weight = (height - 110) * 1.15
   if ideal_weight > 0
-    puts "#{name}, your ideal weight is #{ideal_weight} killograms"
+    puts "#{name}, your ideal weight is #{ideal_weight} killograms."
   elsif heigh = String
     puts "Sorry, I didn't understand you."
   else
