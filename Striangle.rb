39ad954d69b2be7fbac93d54bb7f6e9111c7c0e3
TailsSavarin <57631057+TailsSavarin@users.noreply.github.com>
2020-01-23 00:21:55 +0300

    Delete Striangle.rb

--- Striangle.rb
+++
@@ -1,6 +0,0 @@
-puts "In order to fint out the area of the triangle, enter his base and height in centimeters"
-print "The base is: "
-a = gets.to_i
-print "The height is: "
-h = gets.to_i
-puts "The area of the triangle is: #{(1.0/2)*a*h} centimeters."
