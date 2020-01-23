51348d7e83123df939d412bb22371abaac4ba6d1
TailsSavarin <Alexql7@gmail.com>
2020-01-23 07:57:51 -0800

    1 done

--- Lesson2/number_months.rb
+++ Lesson2/number_months.rb
@@ -1,6 +1,6 @@
 months = { January: 31, February: 29, March: 31, April: 30, May: 31, June: 30, July: 31, August: 31, September: 30, October: 31, November: 30, December: 31 }
 months.each do |months, days| 
  next unless days == 30
-  puts "#{months}: #{days}"
+  puts "#{months}"
  end
 
