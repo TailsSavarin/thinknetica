3238d217aee80d25f67b05cd9d1b4ca8ca5c5eeb
TailsSavarin <Alexql7@gmail.com>
2020-01-24 11:59:09 -0800

    idk 1 done fiexed

--- Lesson2/number_months.rb
+++ Lesson2/number_months.rb
@@ -1,6 +1,5 @@
 months = { January: 31, February: 29, March: 31, April: 30, May: 31, June: 30, July: 31, August: 31, September: 30, October: 31, November: 30, December: 31 }
-months.each do |months, days| 
- next unless days == 30
-  puts "#{months}"
- end
+months_new = months.select { |month, day| day == 30 }
+months_new.to_a.join(", ")
+puts "Months which the total number of days is thirty: #{months_new.keys}"
 
