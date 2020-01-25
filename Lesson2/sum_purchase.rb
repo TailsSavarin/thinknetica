8fea8613eab7c4371d18a3c5a58e2fe494e8d196
TailsSavarin <Alexql7@gmail.com>
2020-01-25 05:07:10 -0800

    6 done

---
+++ Lesson2/sum_purchase.rb
@@ -0,0 +1,37 @@
+#Adding empty basket hash for products
+food_basket = Hash.new(0)
+#Adding total purchase amount atm
+total_sum = 0
+total_sum.to_f
+
+puts 'Hello, to make a purchase, please enter the data below.'
+puts "(when you're dont with your list, just write *stop* in product's name line)"
+
+loop do
+  print "The product's name: "
+  product_name = gets.chomp
+  if product_name == 'stop'
+    break
+  end
+
+  print "The product's price: "
+  product_price = gets.to_f
+  print "The product's count: "
+  product_count = gets.to_f 
+
+#Adding item hash for food_basket
+  products = {price: product_price, count: product_count}
+  food_basket[product_name] = products 
+end
+#Addint currently products in the basket
+puts "Currently in your basket is:"
+puts "Name - [Price, Count]"
+food_basket.each { |x, y| puts "#{x.capitalize} - #{y.values}" }
+
+puts "The final price for each product: Name - price"
+food_basket.each do |key, value|
+  sum_product_price = value[:price] * value[:count]
+  total_sum += sum_product_price
+  puts "#{key.capitalize} - #{sum_product_price}"
+end
+puts "Total purchase amount is: #{total_sum}"
