d44ddd686c46052c8c5cb6fc221c531d7573f3b9
TailsSavarin <Alexql7@gmail.com>
2020-01-25 05:18:04 -0800

    4 fixed, done

--- Lesson2/hash_alphabet.rb
+++ Lesson2/hash_alphabet.rb
@@ -4,4 +4,4 @@ alphabet = ('a'..'z').to_a
 vowels = ['a', 'e', 'i', 'o', 'u', 'y']
 vowels.each { |vowel| vowels_hash[vowel] = alphabet.index(vowel) + 1 }
 puts "Letter - number"
-vowels_hash.each { |x, y| puts "#{x} is #{y}" }
+vowels_hash.each { |x, y| puts "#{x} - #{y}" }
