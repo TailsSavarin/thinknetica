e23f4ed0fa5cc8f951efdfa1f77867fc02337978
TailsSavarin <Alexql7@gmail.com>
2020-02-12 10:15:21 -0800

    fix

--- Lesson6/instance_counter.rb
+++ Lesson6/instance_counter.rb
@@ -12,7 +12,7 @@ module InstanceCounter
     protected
 
     def register_instance
-      self.class.instances ||= nil
+      return nil if self.class.instances ||= 0
       self.class.instances += 1
     end
   end
